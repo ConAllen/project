dc8b1cc0e59f2b8830c60749b72be4dbfe87cf11
Con Allen <allenconallen46@gmail.com>
2016-12-02 14:45:55 +0000

    Add form to sign ins and name to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161201124347) do
+ActiveRecord::Schema.define(version: 20161202141413) do
 
   create_table "listings", force: :cascade do |t|
     t.string   "name"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20161201124347) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
