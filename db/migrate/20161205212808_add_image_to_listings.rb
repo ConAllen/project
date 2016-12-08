5c477a787ed8201895fe9ce55d1c1fb249162781
Con Allen <allenconallen46@gmail.com>
2016-12-08 14:29:12 +0000

    listings / grid

---
+++ db/migrate/20161205212808_add_image_to_listings.rb
@@ -0,0 +1,5 @@
+class AddImageToListings < ActiveRecord::Migration
+  def change
+    add_column :listings, :image_url, :string
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+
+
+#Listing.delete_all
+
+Listing.create(name: "Trek Bike", description: "Cycles", category: "Bikes", price: 150.50, image_url: "Trek.jpg",)
+Listing.create(name: "Nespresso Coffee Machine", description: "This compact coffee machine from Nespresso comes in a stylish blue with 19 bar pressure for excellent bodied coffee.", category: "Electrical", price: 150.50, image_url: "nespresso.jpg",)
+Listing.create(name: "Smartwatch", description: "At its heart - a great watch With an always on colour E-Paper display, up to 10 days of battery life, durable glass lens, and thin, curved ergonomic design you will love wearing Pebble Time anywhere and everywhere.", category: "Smart", price: 35.00, image_url: "Smartwatch",)
 #
+
 # Examples:
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
