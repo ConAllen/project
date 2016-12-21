3d8b058c2a6e29ebb6c864e2c5111d872f30c3ee
Con Allen <allenconallen46@gmail.com>
2016-12-21 18:33:26 +0000

    final commit?

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,7 +5,7 @@ class Order < ActiveRecord::Base
 # every  order belongs to a listing
     belongs_to :listing
 
-# this tells rails that buyer is a type of user and so is seller.
+# this tells rails that a buyer is a type of user and so is seller.
     belongs_to :buyer, class_name: "User"
     belongs_to :seller, class_name: "User"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,13 @@ class User < ActiveRecord::Base
 
 # the below  validation code is to ensure the user enters a name
           validates :name, presence: true
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+          devise :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :trackable, :validatable
 
 
 # this means a listing exsisting, depends on the user. if the user is deleted the listing will also be deleted
          has_many :listings, dependent: :destroy
-         
+
 #this tells rails that a user has many sales. they have orders that they purchased and sales they have sold.
          has_many :sales, class_name: "Order", foreign_key: "seller_id"
          has_many :purchases, class_name: "Order", foreign_key: "buyer_id"
