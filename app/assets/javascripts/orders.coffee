6cec28527912fa9cc8c6f94da9443ca3a2b18200
Con Allen <allenconallen46@gmail.com>
2016-12-21 14:14:41 +0000

    Finalized stripe payment

--- app/assets/javascripts/orders.coffee
+++ app/assets/javascripts/orders.coffee
@@ -1,7 +1,7 @@
 
-#the below code is for stripe validation. Once i enter vallid stripe test creditcard details which are available
+#the below code is for stripe validation. Once i enter vallid stripe test creditcard details "4242424242424242" which are available
 #on their site. and enter it into buy a product. The code below varifys it and stripe sends me a token number.
-#
+# Once you see the token alert which appears after you enter a valid test number. you get an alert with a stripe token
 jQuery ->
   Stripe.setPublishableKey($('meta[name="stripe-key"]').attr('content'))
   payment.setupForm()
@@ -15,6 +15,8 @@ payment =
 
   handleStripeResponse: (status, response) ->
     if status == 200
+      $('#new_order').append($('<input type="hidden" name="stripeToken" />').val(response.id))
+      $('#new_order')[0].submit()
       alert(response.id)
     else
       alert(response.error.message)
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -15,14 +15,19 @@ class OrdersController < ApplicationController
 
   # GET /orders
   # GET /orders.json
-  def index
-    @orders = Order.all
-  end
+
+#i have disabled the
+
+# INDEX AND SHOW ORDER PAGE HAS BEEN DISABLED ORDER PAGE HAS BEEN EDITED SO USERS CAN EDIT THERE PREVIOUS ORDERS
+
+  # def index
+  #  @orders = Order.all
+#  end
 
   # GET /orders/1
   # GET /orders/1.json
-  def show
-  end
+#  def show
+  #end
 
   # GET /orders/new
   def new
@@ -31,8 +36,8 @@ class OrdersController < ApplicationController
   end
 
   # GET /orders/1/edit
-  def edit
-  end
+#  def edit
+#  end
 
   # POST /orders
   # POST /orders.json
@@ -49,9 +54,29 @@ class OrdersController < ApplicationController
     @order.buyer_id = current_user.id
     @order.seller_id = @seller.id
 
+#this first line tells stripe the secret key i added earlier from the stripe site and tells what acc to charge
+    Stripe.api_key = ENV["STRIPE_API_KEY"]
+# looks in the submitted form data. then pulls out the token stipe has given us, and hides it in the token.
+    token = params[:stripeToken]
+
+#this code is available on the stripe API page for charging accounts
+    begin
+      charge = Stripe::Charge.create(
+        :amount => (@listing.price * 100).floor,
+        :currency => "eur",
+        :card => token
+        )
+      flash[:notice] = "Thank you for ordering, Come back soon!"
+    rescue Stripe::CardError => e
+      flash[:danger] = e.message
+    end
+
+
+
+
     respond_to do |format|
       if @order.save
-        format.html { redirect_to root_url, notice: 'Order was successfully created.' }
+        format.html { redirect_to root_url}
         format.json { render :show, status: :created, location: @order }
       else
         format.html { render :new }
@@ -62,27 +87,27 @@ class OrdersController < ApplicationController
 
   # PATCH/PUT /orders/1
   # PATCH/PUT /orders/1.json
-  def update
-    respond_to do |format|
-      if @order.update(order_params)
-        format.html { redirect_to @order, notice: 'Order was successfully updated.' }
-        format.json { render :show, status: :ok, location: @order }
-      else
-        format.html { render :edit }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+#  def update
+  #  respond_to do |format|
+  ###    if @order.update(order_params)
+  #      format.html { redirect_to @order, notice: 'Order was successfully updated.' }
+  #      format.json { render :show, status: :ok, location: @order }
+  #    else
+  #      format.html { render :edit }
+  #      format.json { render json: @order.errors, status: :unprocessable_entity }
+  #    end
+  ##  end
+  # end
 
   # DELETE /orders/1
   # DELETE /orders/1.json
-  def destroy
-    @order.destroy
-    respond_to do |format|
-      format.html { redirect_to orders_url, notice: 'Order was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+#  def destroy
+#    @order.destroy
+  #  respond_to do |format|
+  #    format.html { redirect_to orders_url, notice: 'Order was successfully destroyed.' }
+  #    format.json { head :no_content }
+  #  end
+  # end
 
   private
     # Use callbacks to share common setup or constraints between actions.
